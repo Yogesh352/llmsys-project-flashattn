fix autodiff and implement backprop (might need to optimize block size still) but it works for most cases for now

minitorch/cuda_kernel_ops.py
@@ -603,7 +603,7 @@ class CudaKernelOps(TensorOps):
         #   END ASSIGN3_2
 
     @staticmethod
-    def flash_attention_fw(Q: Tensor, K: Tensor, V: Tensor):  
+    def flash_attention_fw(Q: Tensor, K: Tensor, V: Tensor):
         # print(Q._tensor._storage)
         l = Q.zeros((Q.shape[0], Q.shape[1], Q.shape[2]))
 
@@ -641,22 +641,22 @@ class CudaKernelOps(TensorOps):
 
         lib_flashattention.launch_flashattention_forward.restype = None
 
-        print("Q CHARACTERISTICS")
-        print(Q._tensor.shape)
-        print(Q)
-        print(Q._tensor.is_contiguous())
+        # print("Q CHARACTERISTICS")
+        # print(Q._tensor.shape)
+        # print(Q)
+        # print(Q._tensor.is_contiguous())
         Q = Q.contiguous()
         K = K.contiguous()
         V = V.contiguous()
         # V = V.contiguous()
-        print(Q._tensor.is_contiguous())
+        # print(Q._tensor.is_contiguous())
         # Q_fixed = TensorData(Q._tensor._storage.copy(), Q.shape)
         # print(Q_fixed._tensor.strides)
 
-        print(Q._tensor._storage[0])
-        print(Q._tensor._storage[1])
-        print(Q._tensor._storage[2])
-        print(Q._tensor._storage[3])
+        # print(Q._tensor._storage[0])
+        # print(Q._tensor._storage[1])
+        # print(Q._tensor._storage[2])
+        # print(Q._tensor._storage[3])
 
         lib_flashattention.launch_flashattention_forward(
             Q._tensor._storage,
@@ -671,7 +671,91 @@ class CudaKernelOps(TensorOps):
             d,
         )
 
-        return O
+        return O, m, l
+
+    @staticmethod
+    def flash_attention_bw(
+        Q: Tensor, K: Tensor, V: Tensor, O: Tensor, dO: Tensor, m: Tensor, l: Tensor
+    ):
+        # print(Q._tensor._storage)
+        # print("I REACHED BACKWARD IN CUDA KERNEL OPS")
+        # l = Q.zeros((Q.shape[0], Q.shape[1], Q.shape[2]))
+
+        # print(Q._tensor._storage)
+        B = Q.shape[0]
+        nh = Q.shape[1]
+        N = Q.shape[2]
+        d = Q.shape[3]
+        dQ = Q.zeros((B, nh, N, d))
+        dK = Q.zeros((B, nh, N, d))
+        dV = Q.zeros((B, nh, N, d))
+        # print("cuda kernel ops")
+        # print(Q._tensor._storage)
+        # print(K._tensor._storage)
+        # print(V._tensor._storage)
+
+        # l = Q.((Q.shape[0], Q.shape[1], Q.shape[2]))
+        # inp_grad = inp.zeros(inp.shape)
+
+        # m = tensor_from_numpy(
+        #     np.full((B, nh, N), -np.inf),
+        #     backend=Q.backend,
+        #     requires_grad=True,
+        # )
+        lib_flashattention.launch_flashattention_backward.argtypes = [
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags="C_CONTIGUOUS"),
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+        ]
+
+        lib_flashattention.launch_flashattention_backward.restype = None
+
+        # print("Q CHARACTERISTICS")
+        # print(Q._tensor.shape)
+        # print(Q)
+        # print(Q._tensor.is_contiguous())
+        Q = Q.contiguous()
+        K = K.contiguous()
+        V = V.contiguous()
+        # V = V.contiguous()
+        # print(Q._tensor.is_contiguous())
+        # Q_fixed = TensorData(Q._tensor._storage.copy(), Q.shape)
+        # print(Q_fixed._tensor.strides)
+
+        # print(Q._tensor._storage[0])
+        # print(Q._tensor._storage[1])
+        # print(Q._tensor._storage[2])
+        # print(Q._tensor._storage[3])
+
+        lib_flashattention.launch_flashattention_backward(
+            Q._tensor._storage,
+            K._tensor._storage,
+            V._tensor._storage,
+            O._tensor._storage,
+            dQ._tensor._storage,
+            dK._tensor._storage,
+            dV._tensor._storage,
+            dO._tensor._storage,
+            l._tensor._storage,
+            m._tensor._storage,
+            B,
+            nh,
+            N,
+            d,
+        )
+
+        return dQ, dK, dV
 
     @staticmethod
     def flash_attention_causal_fw(Q: Tensor, K: Tensor, V: Tensor):

minitorch/modules_transfomer.py
@@ -57,6 +57,7 @@ class MultiHeadAttention(Module):
         self.v_projection = Linear(self.n_embd, self.n_embd, bias, backend)
         self.out_projection = Linear(self.n_embd, self.n_embd, bias, backend)
         self.dropout = Dropout(p_dropout)
+        self.use_fused_kernel = use_fused_kernel
         self.use_flash_attention = use_flash_attention
 
     def create_causal_mask(self, bs, nh, seq_len):
@@ -67,7 +68,7 @@ class MultiHeadAttention(Module):
         mask = -np.finfo(datatype).max * np.triu(
             np.ones((bs, nh, seq_len, seq_len), dtype=datatype), 1
         )  # Correct version for Assignment 3.
-        return tensor_from_numpy(mask, backend=self.backend)
+        return tensor_from_numpy(mask, backend=self.backend, requires_grad=True)
 
     def project_to_query_key_value(self, x):
         """Project x to Q, transpose of K, V for self attention
@@ -89,8 +90,9 @@ class MultiHeadAttention(Module):
 
         k = self.k_projection(x.view(*(batch_size * seq_len, n_embd)))
         k = k.view(*(batch_size, seq_len, self.n_head, self.attn_hidden_dim))
+        kT = k.permute(0, 2, 3, 1)
         k = k.permute(0, 2, 1, 3)
-        kT = k.permute(0, 1, 3, 2)
+        # kT = k
         # kT = k
 
         v = self.v_projection(x.view(*(batch_size * seq_len, n_embd)))
@@ -102,7 +104,7 @@ class MultiHeadAttention(Module):
         # print(k)
         # print(v)
 
-        return q,k, kT, v
+        return q, k, kT, v
 
     def self_attention(self, q, kT, v):
         """Given q, kT, and v of sizes defined above, return the result of MultiHeadAttention as described in the writeup
@@ -132,20 +134,20 @@ class MultiHeadAttention(Module):
         result = None
 
         if self.use_fused_kernel:
+
             if self.causal:
                 ### BEGIN YOUR SOLUTION
-                result = (
-                    q.attn_softmax(
-                        (q @ kT) / (self.attn_hidden_dim**0.5)
-                        + self.create_causal_mask(batch_size, num_head, queries_len),
-                        dim=3,
-                    )
-                    @ v
-                )
+                result = ((q @ kT) / (self.attn_hidden_dim**0.5)).attn_softmax(
+                    self.create_causal_mask(batch_size, num_head, queries_len)
+                ) @ v
             else:
-                result = (
-                    q.attn_softmax((q @ kT) / (self.attn_hidden_dim**0.5), dim=3) @ v
-                )
+                result = ((q @ kT) / (self.attn_hidden_dim**0.5)).attn_softmax(
+                    tensor_from_numpy(
+                        np.ones((1, 1, queries_len, queries_len)),
+                        backend=self.backend,
+                        requires_grad=True,
+                    )
+                ) @ v
 
             ### END YOUR SOLUTION
 
@@ -215,11 +217,11 @@ class MultiHeadAttention(Module):
 
         if self.use_flash_attention:
             # There is an upper limit to the per-head dimension that current GPUs (V100, H100) can support.
-            if self.n_embd/self.n_head > 2048:
+            if self.n_embd / self.n_head > 2048:
                 print("Please reduce n_embd or increase n_head")
                 return None
             self_attention = self.self_attention(q, k, v)
-        else: 
+        else:
             self_attention = self.self_attention(q, kT, v)
 
         return self.out_projection(